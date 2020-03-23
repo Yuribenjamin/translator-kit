b2b11fd80410787c463175c30c88db8631127e47
Ibrahim Ragab <Abrahammoustafa@hotmail.co.uk>
2020-03-23 14:28:25 +0200

    x01

--- src/Components/landingpage/Components/Hero.js
+++ src/Components/landingpage/Components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import translatorkit from '../../assets/img/translatorkit.svg';
+import heart from '../../assets/img/heart.svg';
 
 const Container = styled.section``;
 
@@ -10,7 +11,7 @@ const Section = styled.section`
   text-align: center;
   justify-content: center;
   align-items: center;
-  min-height: 31em;
+  min-height: 40em;
 `;
 const Header = styled.header`
   display: flex;
@@ -27,6 +28,23 @@ const Logo = styled.img`
   height: 3em;
   animation: ${rotate} 10s linear infinite;
 `;
+const HeartBeat = keyframes`
+  0% {
+    transform: scale(1.1);
+  }
+  50% {
+    transform: scale(0.8);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+const Heart = styled.img`
+  width: 1em;
+  height: 1em;
+  padding: 0 0.2em 0 0.2em;
+  animation: ${HeartBeat} 1s linear infinite;
+`;
 const P = styled.p`
   color: ${props => props.theme.colors.secondry};
   font-size: 1.5em;
@@ -41,17 +59,26 @@ const H5 = styled.h5`
   font-weight: 400;
   font-size: 0.938em;
 `;
-const Link = styled.a`
+const Button = styled.a`
   padding: 0.938em 2.5em;
   border-radius: 2.5em;
   box-shadow: 0px 1px 8px 1px;
   cursor: pointer;
 `;
+const Footer = styled.footer`
+  display: flex;
+
+  justify-content: center;
+`;
+const Link = styled.a`
+  background: none;
+  text-transform: none;
+`;
 const Hero = () => {
   return (
     <Container role="main">
       <Header>
-        <Logo src={translatorkit} alt="translator kit logo"></Logo>
+        <Logo src={translatorkit} alt="translator kit logo" />
         <P>Translator Kit</P>
       </Header>
       <Section>
@@ -60,8 +87,13 @@ const Hero = () => {
           Translator kit is a free editor for translators, for anyone need
           double editor. start typing now
         </H5>
-        <Link>Try Now</Link>
+        <Button>Wanna Try</Button>
       </Section>
+      <Footer>
+        Made with
+        <Heart src={heart} alt="heart logo" />
+        <Link href="https://twitter.com/AbrahamRagab">Ibrahim Ragab</Link>
+      </Footer>
     </Container>
   );
 };
